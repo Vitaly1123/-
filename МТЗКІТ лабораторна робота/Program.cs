ecfb1ff29b8bd17ab80ee14a5ed73f71dcfca00c
Vitaly1123 <nezhura.vitalii1123@vu.cdu.edu.ua>
2023-11-16 21:26:02 +0200

    first commit

---
+++ МТЗКІТ лабораторна робота/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace МТЗКІТ_лабораторна_робота 
+{
+    internal class Program 
+    {
+        static void Main (string[] args) 
+        {
+        }
+    }
+}
